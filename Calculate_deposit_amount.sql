93e73d9c800f752e7884bacf6c85fa80d147f1c9
FatimaGuliyeva <162604187+FatimaGuliyeva@users.noreply.github.com>
2024-03-12 20:48:25 +0400

    Create Calculate_deposit_amount.sql

---
+++ Calculate_deposit_amount.sql
@@ -0,0 +1,31 @@
+create or replace procedure calc_deposit_profit_amount
+(p_deposit_amount number, p_interest_rate number, p_term number, p_profit_amount out number) is
+begin
+  p_profit_amount := p_deposit_amount*p_interest_rate*p_term/100;
+end;
+
+set serveroutput on;
+declare
+v_profit_amount number;
+begin
+  calc_deposit_profit_amount(50,5,6, v_profit_amount);
+  dbms_output.put_line(v_profit_amount);
+  end;
+  
+  
+  declare
+  v_profit_amount number;
+  v_term number;
+  v_interest_rate number;
+  v_deposit_amount number default 50;
+begin
+   select term, interest_rate into v_term, v_interest_rate
+   from CB_DEPOS?T_PRODUCTS where product_name='novruz emnate';
+   --calc_deposit_profit_amount(50,5,6, v_profit_amount);
+   calc_deposit_profit_amount(v_deposit_amount,v_interest_rate,v_term, v_profit_amount);
+   cb_customer_deposits_insert(1,1,'00009',v_deposit_amount,v_profit_amount, sysdate);
+
+end;
+
+select * from cb_customer_deposits;
+select * from CB_DEPOS?T_PRODUCTS;
